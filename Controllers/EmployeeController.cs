acece23f22ecc7296a2d02914907418b74f34195
Alex Thai <tthai21@student.holmesglen.edu.au>
2023-09-20 23:10:33 +1000

    Fetch all employees

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -35,4 +35,29 @@ public class EmployeeController : ControllerBase
         return NotFound("Failed to add employee");
     }
 
+    [HttpGet(Name = "all-employee")]
+    public async Task<ActionResult<List<Employee>>> FetchAllEmployees()
+    {
+        using var _dbContext = new DataContext();
+        var employees = (from e in _dbContext.Employees
+                         join d in _dbContext.Departments on e.DepartmentId equals d.DepartmentId
+                         join r in _dbContext.Roles on e.RoleId equals r.RoleId
+                         select new EmployeeDTO
+                         {
+                             name = e.EmployeeName,
+                             email = e.EmployeeEmail,
+                             mobile = e.EmployeeMobile,
+                             department = d.DepartmentName,
+                             role = r.RoleName
+                         }).ToList();
+        if (employees != null)
+        {
+            return Ok(employees);
+        }
+        else
+        {
+            return NotFound("Failed to fetch employees");
+        }
+
+    }
 }
